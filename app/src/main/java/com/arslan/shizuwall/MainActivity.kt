a8b05dc5732348d67379cae5e1ade0ae8239f91a
Ahmetcan Arslan <ahmetcanarslan25@gmail.com>
2025-10-26 22:26:07 +0300

    add search bar

--- app/src/main/java/com/arslan/shizuwall/MainActivity.kt
+++ app/src/main/java/com/arslan/shizuwall/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.arslan.shizuwall
 
+import android.annotation.SuppressLint
 import android.content.pm.ApplicationInfo
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -16,7 +18,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var appListAdapter: AppListAdapter
     private lateinit var firewallToggle: SwitchMaterial
+    private lateinit var searchView: SearchView
     private val appList = mutableListOf<AppInfo>()
+    private val filteredAppList = mutableListOf<AppInfo>()
     private var isFirewallEnabled = false
     
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,8 +34,38 @@ class MainActivity : AppCompatActivity() {
         }
         
         setupFirewallToggle()
+        setupSearchView()
         setupRecyclerView()
         loadInstalledApps()
+    }
+    
+    private fun setupSearchView() {
+        searchView = findViewById(R.id.searchView)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                filterApps(newText ?: "")
+                return true
+            }
+        })
+    }
+    
+    @SuppressLint("NotifyDataSetChanged")
+    private fun filterApps(query: String) {
+        filteredAppList.clear()
+        if (query.isEmpty()) {
+            filteredAppList.addAll(appList)
+        } else {
+            val searchQuery = query.lowercase()
+            filteredAppList.addAll(appList.filter { 
+                it.appName.lowercase().contains(searchQuery) || 
+                it.packageName.lowercase().contains(searchQuery)
+            })
+        }
+        appListAdapter.notifyDataSetChanged()
     }
     
     private fun setupFirewallToggle() {
@@ -49,13 +83,14 @@ class MainActivity : AppCompatActivity() {
     private fun setupRecyclerView() {
         recyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        appListAdapter = AppListAdapter(appList) { appInfo ->
+        appListAdapter = AppListAdapter(filteredAppList) { appInfo ->
             // when an app is clicked
             Toast.makeText(this, "Seçilen: ${appInfo.appName}", Toast.LENGTH_SHORT).show()
         }
         recyclerView.adapter = appListAdapter
     }
     
+    @SuppressLint("NotifyDataSetChanged")
     private fun loadInstalledApps() {
         val packageManager = packageManager
         val packages = packageManager.getInstalledApplications(0)
@@ -71,7 +106,13 @@ class MainActivity : AppCompatActivity() {
                 appList.add(AppInfo(appName, packageName, icon))
             }
         }
-        appList.sortBy { it.appName.lowercase() }
+        
+        val turkishCollator = java.text.Collator.getInstance(java.util.Locale.forLanguageTag("tr-TR"))
+        appList.sortWith(compareBy(turkishCollator) { it.appName })
+        
+        filteredAppList.clear()
+        filteredAppList.addAll(appList)
+        
         appListAdapter.notifyDataSetChanged()
     }
 }
