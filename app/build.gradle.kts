68c64378f6c1de5235b342a85ec9b71d20e2f408
Ahmetcan Arslan <ahmetcanarslan25@gmail.com>
2025-11-12 13:18:47 +0300

    bump versioncode to 7, name to 2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.arslan.shizuwall"
         minSdk = 30
         targetSdk = 36
-        versionCode = 5
-        versionName = "2.3"
+        versionCode = 7
+        versionName = "2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
