fc6b1f608dff23b0bf2dbdd6808b61d3a2143722
Ahmetcan Arslan <ahmetcanarslan25@gmail.com>
2025-11-22 12:32:22 +0300

    bump version to 3.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.arslan.shizuwall"
         minSdk = 30
         targetSdk = 36
-        versionCode = 8
-        versionName = "3.0"
+        versionCode = 9
+        versionName = "3.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
