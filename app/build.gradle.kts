db769472b76f8b143ad9ecfeba326438c6b025d7
Ahmetcan Arslan <ahmetcanarslan25@gmail.com>
2026-01-05 22:37:38 +0300

    bump version code to 23 and version name to 4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.arslan.shizuwall"
         minSdk = 30
         targetSdk = 36
-        versionCode = 22
-        versionName = "4.0"
+        versionCode = 23
+        versionName = "4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
