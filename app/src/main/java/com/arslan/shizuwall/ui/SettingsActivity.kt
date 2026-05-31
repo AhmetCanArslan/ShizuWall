9db278cd330eb4af22be4b94b11859314c420ad9
AhmetCanArslan <ahmetcanarslan25@gmail.com>
2026-05-31 18:20:16 +0300

    Remove fdroid flavour and packed into one

--- app/src/main/java/com/arslan/shizuwall/ui/SettingsActivity.kt
+++ app/src/main/java/com/arslan/shizuwall/ui/SettingsActivity.kt
@@ -314,19 +314,6 @@ class SettingsActivity : BaseActivity() {
             else -> radioGroupWorkingMode.check(R.id.radioShizukuMode)
         }
 
-        if (com.arslan.shizuwall.BuildConfig.FLAVOR == "fdroid") {
-            radioLadbMode.isEnabled = false
-            radioLadbMode.alpha = 0.5f
-            val notSupportedText = getString(R.string.ladb_not_supported_fdroid)
-            if (!radioLadbMode.text.toString().contains(notSupportedText)) {
-                radioLadbMode.text = "${radioLadbMode.text}$notSupportedText"
-            }
-            if (radioLadbMode.isChecked) {
-                radioGroupWorkingMode.check(R.id.radioShizukuMode)
-                sharedPreferences.edit().putString(MainActivity.KEY_WORKING_MODE, WorkingMode.SHIZUKU.name).apply()
-            }
-        }
-
         updateWorkingModeDependentUi(workingMode, restoreAutoEnable = false)
     }
     
