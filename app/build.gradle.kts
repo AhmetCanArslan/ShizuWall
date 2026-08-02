237b502e91a7acda76e023fd27ccb1063eea79b2
AhmetCanArslan <ahmetcanarslan25@gmail.com>
2026-08-02 17:04:53 +0300

    Bump version code to 43 and version name to 4.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.arslan.shizuwall"
         minSdk = 30
         targetSdk = 36
-        versionCode = 42
-        versionName = "4.5.4"
+        versionCode = 43
+        versionName = "4.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
